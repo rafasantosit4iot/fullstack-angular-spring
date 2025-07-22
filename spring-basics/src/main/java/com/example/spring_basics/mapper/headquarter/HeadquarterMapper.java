5e34d4382ad9c539d0c21afefa08bf6dc3fbe8e7
rafasantosit4iot <rafael.santos@it4iot.com>
2025-07-22 09:10:42 -0300

    👌Separação de responsabilidades para responseDTO e summaryDTO das sedes

--- spring-basics/src/main/java/com/example/spring_basics/mapper/headquarter/HeadquarterMapper.java
+++ spring-basics/src/main/java/com/example/spring_basics/mapper/headquarter/HeadquarterMapper.java
@@ -1,17 +1,17 @@
 package com.example.spring_basics.mapper.headquarter;
 
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.example.spring_basics.dto.request.headquarter.CreateHeadquarterDTO;
 import com.example.spring_basics.dto.response.country.CountrySummaryDTO;
 import com.example.spring_basics.dto.response.headquarter.HeadquarterResponseDTO;
-import com.example.spring_basics.dto.response.headquarter.HeadquarterSummaryDTO;
 import com.example.spring_basics.dto.response.publisher.PublisherSummaryDTO;
-import com.example.spring_basics.mapper.MappingCordinator;
+import com.example.spring_basics.mapper.country.CountrySummaryConverter;
+import com.example.spring_basics.mapper.publisher.PublisherSummaryConverter;
 import com.example.spring_basics.model.Country;
 import com.example.spring_basics.model.Headquarter;
 import com.example.spring_basics.model.Publisher;
@@ -19,11 +19,11 @@ import com.example.spring_basics.model.Publisher;
 @Component
 public class HeadquarterMapper {
 
-    private final MappingCordinator mappingCordinator;
+    @Autowired
+    CountrySummaryConverter countrySummaryConverter;
 
-    public HeadquarterMapper(MappingCordinator mappingCordinator) {
-        this.mappingCordinator = mappingCordinator;
-    }
+    @Autowired
+    PublisherSummaryConverter publisherSummaryConverter;
 
     public Headquarter toEntity(CreateHeadquarterDTO createHeadquarter, Publisher publisher, Country country) {
         Headquarter headquarter = new Headquarter();
@@ -41,17 +41,14 @@ public class HeadquarterMapper {
         return headquarter;
     }
 
-    // RESPONSE DTO
-    // -------------------------------------------------------------------------------------------
-
     public HeadquarterResponseDTO toResponseDTO(Headquarter headquarter) {
         Integer id = headquarter.getId();
         String name = headquarter.getName();
         String street = headquarter.getStreet();
         String city = headquarter.getCity();
 
-        CountrySummaryDTO countrySummaryDTO = mappingCordinator.toCountrySummary(headquarter.getCountry());
-        PublisherSummaryDTO publisherSummaryDTO = mappingCordinator.toPublisherSummary(headquarter.getPublisher());
+        CountrySummaryDTO countrySummaryDTO = countrySummaryConverter.toSummaryDTO(headquarter.getCountry());
+        PublisherSummaryDTO publisherSummaryDTO = publisherSummaryConverter.toSummaryDTO(headquarter.getPublisher());
 
         HeadquarterResponseDTO headquarterResponseDTO = new HeadquarterResponseDTO(id, name, street, city,
                 countrySummaryDTO, publisherSummaryDTO);
@@ -64,27 +61,4 @@ public class HeadquarterMapper {
                 .map(this::toResponseDTO)
                 .collect(Collectors.toList());
     }
-
-    // SUMMARY DTO
-    // -------------------------------------------------------------------------------------------
-
-    public HeadquarterSummaryDTO toHeadquarterSummaryDTO(Headquarter headquarter) {
-        Integer id = headquarter.getId();
-        String name = headquarter.getName();
-        String street = headquarter.getStreet();
-        String city = headquarter.getCity();
-        CountrySummaryDTO countrySummaryDTO = mappingCordinator.toCountrySummary(headquarter.getCountry());
-
-        HeadquarterSummaryDTO headquarterSummaryDTO = new HeadquarterSummaryDTO(id, name, street, city,
-                countrySummaryDTO);
-
-        return headquarterSummaryDTO;
-    }
-
-    public List<HeadquarterSummaryDTO> toHeadquarterSummaryDTOList(Set<Headquarter> headquarters) {
-        return headquarters.stream()
-                .map(this::toHeadquarterSummaryDTO)
-                .collect(Collectors.toList());
-    }
-
 }
---
+++ spring-basics/src/main/java/com/example/spring_basics/mapper/headquarter/HeadquarterSummaryConverter.java
@@ -0,0 +1,28 @@
+package com.example.spring_basics.mapper.headquarter;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import com.example.spring_basics.dto.response.headquarter.HeadquarterSummaryDTO;
+import com.example.spring_basics.model.Headquarter;
+
+@Component
+public class HeadquarterSummaryConverter {
+    public HeadquarterSummaryDTO toSummaryDTO(Headquarter headquarter) {
+        return new HeadquarterSummaryDTO(
+                headquarter.getId(),
+                headquarter.getName(),
+                headquarter.getStreet(),
+                headquarter.getCity(),
+                headquarter.getCountry().getName());
+    }
+
+    public List<HeadquarterSummaryDTO> toSummaryList(Collection<Headquarter> headquarters) {
+        return headquarters.stream()
+                .map(this::toSummaryDTO)
+                .collect(Collectors.toList());
+    }
+}

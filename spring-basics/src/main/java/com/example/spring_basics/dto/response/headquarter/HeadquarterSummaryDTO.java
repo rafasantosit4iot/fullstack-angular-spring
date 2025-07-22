1bded44bc41d1c24df2b2a34abca6ae00d57a273
rafasantosit4iot <rafael.santos@it4iot.com>
2025-07-22 09:11:26 -0300

    👌Mudança do atributo de objeto do país para o nome do país

--- spring-basics/src/main/java/com/example/spring_basics/dto/response/headquarter/HeadquarterSummaryDTO.java
+++ spring-basics/src/main/java/com/example/spring_basics/dto/response/headquarter/HeadquarterSummaryDTO.java
@@ -1,12 +1,10 @@
 package com.example.spring_basics.dto.response.headquarter;
 
-import com.example.spring_basics.dto.response.country.CountrySummaryDTO;
-
 public record HeadquarterSummaryDTO(
         Integer id,
         String name,
         String street,
         String city,
-        CountrySummaryDTO countrySummaryDTO) {
+        String countryName) {
 
 }

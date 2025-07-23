1c51fad849800329d5b08ca49e2ae03bc895e8d0
rafasandev <rs7951006@gmail.com>
2025-07-22 22:36:11 -0300

    👌Ajuste do campo de validação para o Integer e criação do endpoint de criação de livro

--- spring-basics/src/main/java/com/example/spring_basics/controller/BookController.java
+++ spring-basics/src/main/java/com/example/spring_basics/controller/BookController.java
@@ -3,16 +3,20 @@ package com.example.spring_basics.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.spring_basics.dto.request.book.CreateBookDTO;
+import com.example.spring_basics.dto.response.book.BookResponseDTO;
 import com.example.spring_basics.service.BookService;
 
-import org.springframework.web.bind.annotation.GetMapping;
-
-import com.example.spring_basics.dto.response.book.BookResponseDTO;
+import jakarta.validation.Valid;
 
 
 
@@ -30,4 +34,9 @@ public class BookController {
         return ResponseEntity.ok(books);
     }
     
+    @PostMapping
+    public ResponseEntity<BookResponseDTO> createBook(@Valid @RequestBody CreateBookDTO createBookDTO) {
+        BookResponseDTO bookResponseDTO = bookService.createBook(createBookDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(bookResponseDTO);
+    }
 }

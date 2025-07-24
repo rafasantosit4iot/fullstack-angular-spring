04b632aea55df6dc0895a43649ae1dc3317cf443
rafasantosit4iot <rafael.santos@it4iot.com>
2025-07-24 14:05:52 -0300

    :sparkles: Criação do endpoint de deleção de editora

--- spring-basics/src/main/java/com/example/spring_basics/controller/PublisherController.java
+++ spring-basics/src/main/java/com/example/spring_basics/controller/PublisherController.java
@@ -1,11 +1,13 @@
 package com.example.spring_basics.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,7 @@ import com.example.spring_basics.service.PublisherService;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.spring_basics.dto.response.publisher.PublisherResponseDTO;
 
@@ -41,6 +44,12 @@ public class PublisherController {
             @Valid @RequestBody CreatePublisherDTO createPublisherDTO) {
         PublisherResponseDTO responseDTO = publisherService.createPublisher(createPublisherDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePublisher(@PathVariable UUID id) {
+        this.publisherService.deletePublisher(id);
+        return ResponseEntity.ok().build();
     }
 
 }

697151d9655c38952a1461b3820bd604733d048f
rafasantosit4iot <rafael.santos@it4iot.com>
2025-07-25 09:52:27 -0300

    ✨ Criação das entidades de mapeamento e conversão dos Empréstimos

--- spring-basics/src/main/java/com/example/spring_basics/mapper/loan/LoanSummaryConverter.java
+++ spring-basics/src/main/java/com/example/spring_basics/mapper/loan/LoanSummaryConverter.java
@@ -1,5 +1,9 @@
 package com.example.spring_basics.mapper.loan;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Component;
 
 import com.example.spring_basics.dto.response.loan.LoanSummaryDTO;
@@ -8,8 +12,15 @@ import com.example.spring_basics.model.Loan;
 @Component
 public class LoanSummaryConverter {
     public LoanSummaryDTO toSummaryDTO(Loan loan) {
-        LoanSummaryDTO loanSummaryDTO = new LoanSummaryDTO(
-            
-        );
+        return new LoanSummaryDTO(
+                loan.getId(),
+                loan.getLoanDate(),
+                loan.getStatus());
+    }
+
+    public List<LoanSummaryDTO> toSummaryList(Collection<Loan> loans) {
+        return loans.stream()
+                .map(this::toSummaryDTO)
+                .collect(Collectors.toList());
     }
 }

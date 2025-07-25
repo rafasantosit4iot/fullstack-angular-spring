b7989a98b02ed9c885e2c6c308bbeb9c2568dea6
rafasantosit4iot <rafael.santos@it4iot.com>
2025-07-25 09:51:13 -0300

    ✨ Criação das entidades de resposta dos empréstimos

--- spring-basics/src/main/java/com/example/spring_basics/dto/response/loan/LoanSummaryDTO.java
+++ spring-basics/src/main/java/com/example/spring_basics/dto/response/loan/LoanSummaryDTO.java
@@ -1,7 +1,13 @@
 package com.example.spring_basics.dto.response.loan;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
+import com.example.spring_basics.model.enums.LoanStatus;
+
 public record LoanSummaryDTO(
-    
-) {
-    
+        UUID id,
+        LocalDate loanDate,
+        LoanStatus status) {
+
 }

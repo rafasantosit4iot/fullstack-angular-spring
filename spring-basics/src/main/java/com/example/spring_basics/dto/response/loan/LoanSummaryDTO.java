274ca30acb9b69e9e7c304da2d7b1d6591211f2c
rafasandev <rs7951006@gmail.com>
2025-07-26 19:27:45 -0300

    ✨Criação da entidade de summary do Loan

--- spring-basics/src/main/java/com/example/spring_basics/dto/response/loan/LoanSummaryDTO.java
+++ spring-basics/src/main/java/com/example/spring_basics/dto/response/loan/LoanSummaryDTO.java
@@ -1,7 +1,17 @@
 package com.example.spring_basics.dto.response.loan;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
+import com.example.spring_basics.model.enums.LoanStatus;
+
 public record LoanSummaryDTO(
-    
-) {
-    
+        UUID id,
+        String bookClassificationCode,
+        String userName,
+        LocalDate loanDate,
+        LocalDate dueDate,
+        LoanStatus status
+        ) {
+
 }

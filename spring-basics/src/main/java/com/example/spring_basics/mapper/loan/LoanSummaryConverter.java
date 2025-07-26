c86f771ef00fd72d796ba9ddca1dc20e6b2c45a4
rafasandev <rs7951006@gmail.com>
2025-07-26 19:28:36 -0300

    ✨Criação dos métodos de conversão do loan para lista de summary

--- spring-basics/src/main/java/com/example/spring_basics/mapper/loan/LoanSummaryConverter.java
+++ spring-basics/src/main/java/com/example/spring_basics/mapper/loan/LoanSummaryConverter.java
@@ -1,5 +1,9 @@
 package com.example.spring_basics.mapper.loan;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Component;
 
 import com.example.spring_basics.dto.response.loan.LoanSummaryDTO;
@@ -7,9 +11,22 @@ import com.example.spring_basics.model.Loan;
 
 @Component
 public class LoanSummaryConverter {
+
     public LoanSummaryDTO toSummaryDTO(Loan loan) {
         LoanSummaryDTO loanSummaryDTO = new LoanSummaryDTO(
-            
-        );
+                loan.getId(),
+                loan.getBookCopy().getClassificationCode(),
+                loan.getUser().getName(),
+                loan.getLoanDate(),
+                loan.getDueDate(),
+                loan.getStatus());
+
+        return loanSummaryDTO;
+    }
+
+    public List<LoanSummaryDTO> toSummaryList(Collection<Loan> loans) {
+        return loans.stream()
+                .map(this::toSummaryDTO)
+                .collect(Collectors.toList());
     }
 }

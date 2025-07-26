a0a1fd1787fe3131bcd3d11cdb91a27b5f1e06ba
rafasandev <rs7951006@gmail.com>
2025-07-26 19:28:10 -0300

    👌Finalização do método de mapeamento da cópia de livro

--- spring-basics/src/main/java/com/example/spring_basics/mapper/book_copy/BookCopyMapper.java
+++ spring-basics/src/main/java/com/example/spring_basics/mapper/book_copy/BookCopyMapper.java
@@ -7,14 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.example.spring_basics.dto.request.book_copy.CreateBookCopyDTO;
-import com.example.spring_basics.dto.response.book.BookResponseDTO;
 import com.example.spring_basics.dto.response.book.BookSummaryDTO;
 import com.example.spring_basics.dto.response.book_copy.BookCopyResponseDTO;
+import com.example.spring_basics.dto.response.loan.LoanSummaryDTO;
 import com.example.spring_basics.mapper.book.BookSummaryConverter;
 import com.example.spring_basics.mapper.loan.LoanSummaryConverter;
 import com.example.spring_basics.model.Book;
 import com.example.spring_basics.model.BookCopy;
-import com.example.spring_basics.model.Loan;
+import com.example.spring_basics.model.enums.CopyStatus;
 
 @Component
 public class BookCopyMapper {
@@ -41,12 +41,9 @@ public class BookCopyMapper {
         CopyStatus status = bookCopy.getStatus();
         
         BookSummaryDTO bookSummaryDTO = bookSummaryConverter.toSummaryDTO(bookCopy.getBook());
+        List<LoanSummaryDTO> loans = loanSummaryConverter.toSummaryList(bookCopy.getLoans());
 
-        List<Loan> loans = 
-
-        BookCopyResponseDTO bookCopyResponseDTO = new BookCopyResponseDTO();
-
-
-
+        BookCopyResponseDTO bookCopyResponseDTO = new BookCopyResponseDTO(id, classificationCode, status, bookSummaryDTO, loans);
+        return bookCopyResponseDTO;
     }
 }

3eb02d3b23a498a82b45b7f80a67f2e2b292e027
rafasantosit4iot <rafael.santos@it4iot.com>
2025-07-28 17:46:25 -0300

    ✨Criação dos Services de usuário

--- spring-basics/src/main/java/com/example/spring_basics/service/user/UserService.java
+++ spring-basics/src/main/java/com/example/spring_basics/service/user/UserService.java
@@ -1,5 +1,12 @@
 package com.example.spring_basics.service.user;
 
-public class UserService {
-    
+import java.util.List;
+
+import com.example.spring_basics.dto.request.user.CreateUserDTO;
+import com.example.spring_basics.dto.response.user.UserResponseDTO;
+
+public interface UserService {
+    public UserResponseDTO createUser(CreateUserDTO createUserDTO);
+
+    public List<UserResponseDTO> getAllUsers();
 }
--- spring-basics/src/main/java/com/example/spring_basics/service/user/UserServiceImpl.java
+++ spring-basics/src/main/java/com/example/spring_basics/service/user/UserServiceImpl.java
@@ -1,5 +1,36 @@
 package com.example.spring_basics.service.user;
 
-public class UserServiceImpl {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.spring_basics.dto.request.user.CreateUserDTO;
+import com.example.spring_basics.dto.response.user.UserResponseDTO;
+import com.example.spring_basics.mapper.user.UserMapper;
+import com.example.spring_basics.model.User;
+import com.example.spring_basics.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserMapper userMapper;
+    private final UserRepository userRepository;
+    private final UserLibraryCode userLibraryCode;
+
+    @Override
+    public UserResponseDTO createUser(CreateUserDTO createUserDTO) {
+        String libraryCode = userLibraryCode.generateLibraryUserCode(createUserDTO.name());
+
+        User user = userMapper.toEntity(createUserDTO, libraryCode);
+        user = userRepository.save(user);
+        return userMapper.toResponseDTO(user);
+    }
     
+    @Override
+    public List<UserResponseDTO> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return userMapper.toResponseDTOList(users);
+    }
 }

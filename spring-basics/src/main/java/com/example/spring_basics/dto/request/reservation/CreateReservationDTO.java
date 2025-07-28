5b7b16ee2e4f74eb89d79f8d3836a90670adbfaa
rafasantosit4iot <rafael.santos@it4iot.com>
2025-07-28 17:44:56 -0300

    👌Validação e iniciação de dados dentro da Request de uma Reserva

--- spring-basics/src/main/java/com/example/spring_basics/dto/request/reservation/CreateReservationDTO.java
+++ spring-basics/src/main/java/com/example/spring_basics/dto/request/reservation/CreateReservationDTO.java
@@ -2,10 +2,19 @@ package com.example.spring_basics.dto.request.reservation;
 
 import java.util.UUID;
 
+import com.example.spring_basics.model.enums.ReservationStatus;
+
+import jakarta.validation.constraints.NotNull;
+
 public record CreateReservationDTO(
-    UUID bookId,
-    UUID userId,
-    UUID loanId
+    @NotNull(message="O ID do livro é obrigatório") UUID bookCopyId,
+    ReservationStatus status,
+    @NotNull(message="O ID do xx é obrigatório") UUID userId,
+    @NotNull(message="O ID do xx é obrigatório") UUID loanId
 ) {
-    
+    public CreateReservationDTO {
+        if (status == null) {
+            status = ReservationStatus.ACTIVE;
+        }
+    }    
 }

8740fcffec4baaa7f6530f0b6069ab2cd79ca90a
rafasandev <rs7951006@gmail.com>
2025-07-27 23:47:08 -0300

    ✨Criação dos services para as cópias de livros

--- spring-basics/src/main/java/com/example/spring_basics/service/book_copy/BookCopyService.java
+++ spring-basics/src/main/java/com/example/spring_basics/service/book_copy/BookCopyService.java
@@ -1,5 +1,12 @@
 package com.example.spring_basics.service.book_copy;
 
-public class BookCopyService {
-    
+import java.util.List;
+
+import com.example.spring_basics.dto.request.book_copy.CreateBookCopyDTO;
+import com.example.spring_basics.dto.response.book_copy.BookCopyResponseDTO;
+
+public interface BookCopyService {
+    public BookCopyResponseDTO createBookCopy(CreateBookCopyDTO createBookCopyDTO);
+
+    public List<BookCopyResponseDTO> getAllBookCopies();
 }
--- spring-basics/src/main/java/com/example/spring_basics/service/book_copy/BookCopyServiceImpl.java
+++ spring-basics/src/main/java/com/example/spring_basics/service/book_copy/BookCopyServiceImpl.java
@@ -1,5 +1,46 @@
 package com.example.spring_basics.service.book_copy;
 
-public class BookCopyServiceImpl {
-    
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.spring_basics.dto.request.book_copy.CreateBookCopyDTO;
+import com.example.spring_basics.dto.response.book_copy.BookCopyResponseDTO;
+import com.example.spring_basics.mapper.book_copy.BookCopyMapper;
+import com.example.spring_basics.model.Book;
+import com.example.spring_basics.model.BookCopy;
+import com.example.spring_basics.model.enums.CopyStatus;
+import com.example.spring_basics.repository.BookCopyRepository;
+import com.example.spring_basics.repository.BookRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class BookCopyServiceImpl implements BookCopyService {
+
+    @Autowired
+    BookCopyRepository bookCopyRepository;
+    BookCopyMapper bookCopyMapper;
+    BookRepository bookRepository;
+
+    @Override
+    public BookCopyResponseDTO createBookCopy(CreateBookCopyDTO createBookCopyDTO) {
+        Book book = bookRepository.findById(createBookCopyDTO.bookId())
+                .orElseThrow(() -> new EntityNotFoundException("Livro não encontrado"));
+
+        CopyStatus status = CopyStatus.AVAILABLE;
+        String classificationCode = "codteste";
+
+        BookCopy bookCopy = bookCopyMapper.toEntity(createBookCopyDTO, book, classificationCode, status);
+        bookCopy = bookCopyRepository.save(bookCopy);
+        return bookCopyMapper.toResponseDTO(bookCopy);
+    }
+
+    @Override
+    public List<BookCopyResponseDTO> getAllBookCopies() {
+        List<BookCopy> bookCopies = bookCopyRepository.findAll();
+        return bookCopyMapper.toResponseDTOList(bookCopies);
+    }
+
 }

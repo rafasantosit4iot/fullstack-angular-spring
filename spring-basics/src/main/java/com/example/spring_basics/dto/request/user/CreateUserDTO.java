15ae46f812c18984ee21ded4fdd64a58b2f1ab02
rafasantosit4iot <rafael.santos@it4iot.com>
2025-07-28 17:47:34 -0300

    👌Organização e iniciação de dados para usuário na Request

--- spring-basics/src/main/java/com/example/spring_basics/dto/request/user/CreateUserDTO.java
+++ spring-basics/src/main/java/com/example/spring_basics/dto/request/user/CreateUserDTO.java
@@ -1,7 +1,20 @@
 package com.example.spring_basics.dto.request.user;
 
-public record CreateUserDTO(
-        String name,
-        String email) {
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
 
+public record CreateUserDTO(
+                @NotBlank(message = "O nome do usuário é obrigatório") String name,
+                @NotBlank(message = "O email do usuário é obrigatório") String email,
+                @Max(value=10, message="Um usuário pode ter no máximo 10 empréstimos ao mesmo tempo")Integer maxActiveLoans,
+                Boolean active) {
+
+        public CreateUserDTO {
+                if (maxActiveLoans == null) {
+                        maxActiveLoans = 2;
+                }
+                if (active == null) {
+                        active = true;
+                }
+        }
 }

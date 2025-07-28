e631897cbd293c8894aa86aba5d01c0288450a0a
rafasandev <rs7951006@gmail.com>
2025-07-27 23:57:10 -0300

    ✨Criação do controller e endpoint de getAll das cópias de livros

--- spring-basics/src/main/java/com/example/spring_basics/controller/BookCopyController.java
+++ spring-basics/src/main/java/com/example/spring_basics/controller/BookCopyController.java
@@ -1,5 +1,29 @@
 package com.example.spring_basics.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.spring_basics.dto.response.book_copy.BookCopyResponseDTO;
+import com.example.spring_basics.service.book_copy.BookCopyService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("book_copies")
 public class BookCopyController {
     
+    @Autowired
+    BookCopyService bookCopyService;
+
+    @GetMapping
+    public ResponseEntity<List<BookCopyResponseDTO>> getAllBookCopies() {
+        List<BookCopyResponseDTO> bookCopies = bookCopyService.getAllBookCopies();
+        return ResponseEntity.ok(bookCopies);
+    }
+
 }

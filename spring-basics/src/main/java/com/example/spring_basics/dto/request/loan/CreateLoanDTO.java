7dccf72c1ca6920e3b9b608d55c69842a7596a83
rafasantosit4iot <rafael.santos@it4iot.com>
2025-07-28 17:44:31 -0300

    👌Obrigatoriedade dos dados de livro e usuário para criar um empréstimo

--- spring-basics/src/main/java/com/example/spring_basics/dto/request/loan/CreateLoanDTO.java
+++ spring-basics/src/main/java/com/example/spring_basics/dto/request/loan/CreateLoanDTO.java
@@ -2,9 +2,11 @@ package com.example.spring_basics.dto.request.loan;
 
 import java.util.UUID;
 
+import jakarta.validation.constraints.NotNull;
+
 public record CreateLoanDTO(
-    UUID bookCopyId,
-    UUID userId
+    @NotNull(message="O ID da cópia do livro é obrigatório") UUID bookCopyId,
+    @NotNull(message="O ID do usuário é obrigatório") UUID userId
 ) {
     
 }

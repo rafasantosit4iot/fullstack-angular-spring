9a7afeec3e481e39a31eee8413ada0dd9f767cbc
rafasandev <rs7951006@gmail.com>
2025-07-27 23:57:44 -0300

    👌Adição do método de conversão para lista de resposta de summary para o autor

--- spring-basics/src/main/java/com/example/spring_basics/mapper/author/AuthorSummaryConverter.java
+++ spring-basics/src/main/java/com/example/spring_basics/mapper/author/AuthorSummaryConverter.java
@@ -1,5 +1,9 @@
 package com.example.spring_basics.mapper.author;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Component;
 
 import com.example.spring_basics.dto.response.author.AuthorSummaryDTO;
@@ -7,10 +11,17 @@ import com.example.spring_basics.model.Author;
 
 @Component
 public class AuthorSummaryConverter {
+
     public AuthorSummaryDTO toSummaryDTO(Author author) {
         return new AuthorSummaryDTO(
                 author.getId(),
                 author.getName(),
-                author.getOriginCountry().getId());
+                author.getOriginCountry().getName());
+    }
+
+    public List<AuthorSummaryDTO> toSummaryList(Collection<Author> authors) {
+        return authors.stream()
+                .map(this::toSummaryDTO)
+                .collect(Collectors.toList());
     }
 }

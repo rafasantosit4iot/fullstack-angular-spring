f08923404f2817d6259b9c3b31b66542abe98698
rafasandev <rs7951006@gmail.com>
2025-07-29 00:34:15 -0300

    ✨Criação do Get e Post das reservas

--- spring-basics/src/main/java/com/example/spring_basics/controller/ReservationController.java
+++ spring-basics/src/main/java/com/example/spring_basics/controller/ReservationController.java
@@ -1,5 +1,40 @@
 package com.example.spring_basics.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.spring_basics.dto.request.reservation.CreateReservationDTO;
+import com.example.spring_basics.dto.response.reservation.ReservationResponseDTO;
+import com.example.spring_basics.service.reservation.ReservationService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("reservations")
 public class ReservationController {
-    
+
+    @Autowired
+    ReservationService reservationService;
+
+    @GetMapping
+    public ResponseEntity<List<ReservationResponseDTO>> getAllReservations() {
+        List<ReservationResponseDTO> reservations = reservationService.getAllReservations();
+        return ResponseEntity.ok(reservations);
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservationResponseDTO> createReservation(@Valid @RequestBody CreateReservationDTO createReservationDTO) {
+        ReservationResponseDTO reservationResponseDTO = reservationService.createReservation(createReservationDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(reservationResponseDTO);
+    }
 }

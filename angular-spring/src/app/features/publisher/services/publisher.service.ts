6c8506e83fc35372b91b49d386b8ccfdd36ba0bc
rafasantosit4iot <rafael.santos@it4iot.com>
2025-07-29 17:22:33 -0300

    👌Configuração de signals no Service de editoras

--- angular-spring/src/app/features/publisher/services/publisher.service.ts
+++ angular-spring/src/app/features/publisher/services/publisher.service.ts
@@ -1,24 +1,102 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { effect, inject, Injectable, signal } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment.development';
+import { PublisherCreateBody, PublisherResponse, PublisherResponseItem } from '../models/publisher';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PublisherService {
-  API_URL = 'http://localhost:8080/publishers';
+  // ENVIRONMENTS
+  private API_URL = `${environment.libraryUrl.baseUrl}/publishers`;
 
-  constructor(private http: HttpClient) { }
+  // DEPENDÊNCIAS
+  private http = inject(HttpClient);
 
-  public getAllPublishers() {
-    return this.http.get(this.API_URL);
+  // GÊNEROS COMPLETOS
+  private _publishers = signal<PublisherResponseItem[]>([]);
+  readonly publishers = this._publishers.asReadonly();
+
+  // PAGINAÇÃO
+  private _pageNumber = signal<number>(0);
+  readonly pageNumber = this._pageNumber.asReadonly();
+
+  private _pageSize = signal<number>(12);
+  readonly pageSize = this._pageSize.asReadonly();
+
+  // LOADING
+  private _loading = signal(false);
+  readonly loading = this._loading.asReadonly();
+
+  // ERROR
+  private _error = signal<string | null>(null);
+  readonly error = this._error.asReadonly();
+
+  constructor() {
+    effect(() => {
+      this.getPublishers();
+    })
   }
 
-  public createPublisher(publisher: any): Observable<any> {
-    return this.http.post(this.API_URL, publisher);
+  public setPageNumber(newPageNumber: number) {
+    this._pageNumber.set(newPageNumber);
   }
 
-  public deletePublisher(publisherId: string) {
-    return this.http.delete(`${this.API_URL}/${publisherId}`);
+  public getPublishers(): void {
+    this._loading.set(true);
+    this._error.set(null);
+
+    const pageNumber = this._pageNumber();
+    const pageSize = this._pageSize();
+
+    this.http.get<PublisherResponse>(`${this.API_URL}?pageNumber=${pageNumber}&pageSize=${pageSize}`)
+      .subscribe({
+        next: (apiResponse: PublisherResponse) => {
+          const publishersResult: PublisherResponseItem[] = apiResponse.content;
+          this._publishers.set(publishersResult);
+          this._loading.set(false);
+          console.log(publishersResult);
+        },
+        error: (error: HttpErrorResponse) => {
+          console.error("Erro ao carregar as editoras", error);
+          this._error.set("Erro ao carregar as editoras: " + error.message);
+        }
+      });
+  }
+
+  public createPublisher(newPublisher: PublisherCreateBody): void {
+    this._loading.set(true);
+    this._error.set(null);
+
+    this.http.post<PublisherResponseItem>(this.API_URL, newPublisher)
+      .subscribe({
+        next: (createResponse: PublisherResponseItem) => {
+          this._publishers.update((publishers: PublisherResponseItem[]) => [...publishers, createResponse]);
+          this._loading.set(false);
+          console.log("Criado", createResponse);
+        },
+        error: (error: HttpErrorResponse) => {
+          console.error("Erro ao criar editora", error);
+          this._error.set("Erro ao criar editora: " + error.message);
+        }
+      })
+  }
+
+  public deletePublisher(publisherId: string): void {
+    this._loading.set(true);
+    this._error.set(null);
+
+    this.http.delete<void>(`${this.API_URL}/${publisherId}`)
+      .subscribe({
+        next: (deleteResponse) => {
+          this._loading.set(false);
+          console.log(deleteResponse);
+        },
+        error: (error: HttpErrorResponse) => {
+          console.error("Erro ao excluir editora", error);
+          this._error.set("Erro ao excluir editora: " + error.message);
+        }
+      });
   }
 }

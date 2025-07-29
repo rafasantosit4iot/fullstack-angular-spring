2a3f3ba391a4947ed03c404eac54001eefba9660
rafasantosit4iot <rafael.santos@it4iot.com>
2025-07-29 17:22:10 -0300

    👌Configuração de signals no Service de países

--- angular-spring/src/app/features/country/services/country.service.ts
+++ angular-spring/src/app/features/country/services/country.service.ts
@@ -1,19 +1,84 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { effect, inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../../../environments/environment.development';
+import { CountryCreateBody, CountryResponse, CountryResponseItem } from '../models/country';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
-  API_URL: string = 'http://localhost:8080/countries'
+  // ENVIRONMENTS
+  private API_URL: string = `${environment.libraryUrl.baseUrl}/countries`;
 
-  constructor(private http: HttpClient) { }
+  // DEPENDÊNCIAS
+  private http = inject(HttpClient);
 
-  public getAllCountries() {
-    return this.http.get(this.API_URL);
+  // PAÍSES COMPLETOS
+  private _countries = signal<CountryResponseItem[]>([]);
+  readonly countries = this._countries.asReadonly();
+
+  // PAGINAÇÃO
+  private _pageNumber = signal<number>(0);
+  readonly pageNumber = this._pageNumber.asReadonly();
+
+  private _pageSize = signal<number>(12);
+  readonly pageSize = this._pageSize.asReadonly();
+
+  // LOADING
+  private _loading = signal(false);
+  readonly loading = this._loading.asReadonly();
+
+  // ERROR
+  private _error = signal<string | null>(null);
+  readonly error = this._error.asReadonly();
+
+  constructor() {
+    effect(() => {
+      this.getCountries();
+    })
   }
 
-  public createCountry(country: any) {
-    return this.http.post(this.API_URL, country);
+  public setPageNumber(newPageNumber: number): void {
+    this._pageNumber.set(newPageNumber);
+  }
+
+  public getCountries(): void {
+    this._loading.set(true);
+    this._error.set(null);
+
+    const pageNumber = this._pageNumber();
+    const pageSize = this._pageSize();
+
+    this.http.get<CountryResponse>(`${this.API_URL}?pageNumber=${pageNumber}&pageSize=${pageSize}`)
+      .subscribe({
+        next: (apiResponse: CountryResponse) => {
+          const countriesResult: CountryResponseItem[] = apiResponse.content;
+          this._countries.set(countriesResult);
+          this._loading.set(false);
+          console.log(countriesResult);
+        },
+        error: (error: HttpErrorResponse) => {
+          console.error("Erro ao carregar os países", error);
+          this._error.set("Erro ao carregar os países: " + error.message);
+        }
+      });
+  }
+
+  public createCountry(newCountry: CountryCreateBody): void {
+    this._loading.set(true);
+    this._error.set(null);
+
+    this.http.post<CountryResponseItem>(this.API_URL, newCountry)
+      .subscribe({
+        next: (createResponse: CountryResponseItem) => {
+          this._countries.update((countries: CountryResponseItem[]) => [...countries, createResponse]);
+          this._loading.set(false);
+          console.log("Criado", createResponse);
+        },
+        error: (error: HttpErrorResponse) => {
+          console.error("Erro ao criar país", error);
+          this._error.set("Erro ao criar país: " + error.message);
+        }
+      })
   }
 }

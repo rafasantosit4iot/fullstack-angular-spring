f51f76a9d1f19a318dab7d79afda34a27c3536e6
rafasandev <rs7951006@gmail.com>
2025-07-29 00:38:22 -0300

    ✨ Criação do Get e Post de usuários

--- spring-basics/src/main/java/com/example/spring_basics/controller/UserController.java
+++ spring-basics/src/main/java/com/example/spring_basics/controller/UserController.java
@@ -1,5 +1,39 @@
 package com.example.spring_basics.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.spring_basics.dto.request.user.CreateUserDTO;
+import com.example.spring_basics.dto.response.user.UserResponseDTO;
+import com.example.spring_basics.service.user.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("users")
 public class UserController {
-    
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
+        List<UserResponseDTO> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> createUSer(@Valid @RequestBody CreateUserDTO createUserDTO) {
+        UserResponseDTO users = userService.createUser(createUserDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(users);
+    }
 }

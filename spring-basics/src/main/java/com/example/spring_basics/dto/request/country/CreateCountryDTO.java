c5d297c43dd28445457118c1f05b8d07ab238b4c
rafasantosit4iot <rafael.santos@it4iot.com>
2025-07-30 17:34:29 -0300

    👌Ajuste da mensagem de aviso da classe de Request para países

--- spring-basics/src/main/java/com/example/spring_basics/dto/request/country/CreateCountryDTO.java
+++ spring-basics/src/main/java/com/example/spring_basics/dto/request/country/CreateCountryDTO.java
@@ -6,5 +6,5 @@ import jakarta.validation.constraints.NotBlank;
 
 public record CreateCountryDTO(
         @NotBlank(message = "O nome do país é obrigatório") String name,
-        @Length(min = 3, max = 3, message = "O código de ISO deve ter 2 caracteres") String isoCode) {
+        @Length(min = 3, max = 3, message = "O código de ISO deve ter 3 caracteres") String isoCode) {
 }

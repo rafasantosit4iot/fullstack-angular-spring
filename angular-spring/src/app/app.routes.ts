d569c879be79496a1191026ac485e4bc0348ab2d
rafasandev <rs7951006@gmail.com>
2025-07-31 22:58:30 -0300

    ✨Implementação do roteamento inicial do projeto

--- angular-spring/src/app/app.routes.ts
+++ angular-spring/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/components/home/home.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "home",
+        component: HomeComponent,
+        title: "Home"
+    },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: "**",
+        component: NotFoundComponent
+    }
+];

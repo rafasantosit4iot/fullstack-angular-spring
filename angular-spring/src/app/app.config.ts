47d144d35860c2f9999348ed5e6e712e3bf1870b
rafasantosit4iot <rafael.santos@it4iot.com>
2025-08-01 17:42:47 -0300

    👌Importação do pacote de ícones

--- angular-spring/src/app/app.config.ts
+++ angular-spring/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideNgIconsConfig } from '@ng-icons/core';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -8,6 +9,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideNgIconsConfig({
+      size: '1.5em'
+    })
   ]
 };
